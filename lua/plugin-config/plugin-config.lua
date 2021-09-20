5f15668d60ace36ee17739185f81ced129393855
Bas van de Louw <basvdlouw@gmail.com>
2021-09-21 01:34:21 +0200

    add Java LSP

--- lua/plugin-config/plugin-config.lua
+++ lua/plugin-config/plugin-config.lua
@@ -4,3 +4,11 @@ require('plugin-config/cmp')
 require('plugin-config/telescope')
 require('plugin-config/gitsigns')
 require('plugin-config/lsp')
+
+-- Java LSP
+require('plugin-config/jdtls_setup')
+vim.api.nvim_command([[
+augroup jdtls_lsp
+autocmd FileType java lua require'jdtls_setup'.setup()
+augroup END 
+]])
